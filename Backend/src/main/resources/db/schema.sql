7e4234b056c3b9f7a1b4348f8ff886ecfe5f4477
Asilaaa <147584814+Asilaaa@users.noreply.github.com>
2025-04-22 21:00:12 +0300

    Update schema.sql

--- Backend/src/main/resources/db/schema.sql
+++ Backend/src/main/resources/db/schema.sql
@@ -1,34 +1,35 @@
 DROP TABLE IF EXISTS orders, wishing_list, books, users CASCADE;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    photo TEXT,
-    coins INTEGER DEFAULT 0
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       photo TEXT,
+                       coins INTEGER DEFAULT 0
 );
 
 CREATE TABLE books (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    description TEXT,
-    cover TEXT,
-    file TEXT,
-    genres TEXT[]
+                       id SERIAL PRIMARY KEY,
+                       title VARCHAR(255) NOT NULL,
+                       author VARCHAR(255) NOT NULL,
+                       description TEXT,
+                       cover TEXT,
+                       file TEXT,
+                       genres TEXT
 );
 
 CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
-    ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                        id SERIAL PRIMARY KEY,
+                        user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+                        book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
+                        ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE wishing_list (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255)
+                              id SERIAL PRIMARY KEY,
+                              user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+                              title VARCHAR(255) NOT NULL,
+                              author VARCHAR(255),
+                              book_id INTEGER REFERENCES books(id) ON DELETE CASCADE  -- Add this line
 );
